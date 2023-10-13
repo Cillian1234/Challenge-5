018a6e15fb3b0134354dd4b3a938055feb844fe3
Cillian <126096645+CillianSmee@users.noreply.github.com>
2023-10-13 03:07:48 +0100

    Challenge Complete

--- Assets/Challenge 5/Scripts/GameManagerX.cs
+++ Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -9,6 +9,7 @@ public class GameManagerX : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI timer;
     public GameObject titleScreen;
     public Button restartButton; 
 
@@ -16,6 +17,7 @@ public class GameManagerX : MonoBehaviour
 
     private int score;
     private float spawnRate = 1.5f;
+    private float time = 60;
     public bool isGameActive;
 
     private float spaceBetweenSquares = 2.5f; 
@@ -23,9 +25,9 @@ public class GameManagerX : MonoBehaviour
     private float minValueY = -3.75f; //  y value of the center of the bottom-most square
     
     // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
-    public void StartGame()
+    public void StartGame(int difficulty)
     {
-        spawnRate /= 5;
+        spawnRate /= difficulty;
         isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
@@ -70,15 +72,27 @@ public class GameManagerX : MonoBehaviour
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-        scoreText.text = "score";
+        scoreText.text = "score: " + score;
     }
 
     // Stop game, bring up game over text and restart button
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
-        restartButton.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(true);
         isGameActive = false;
+    }
+
+    void Update()
+    {
+        time -= Time.deltaTime;
+        timer.text = "Time: " + Mathf.Round(time);
+
+        if (time <= 0)
+        {
+            GameOver();
+            timer.text = "Time: :(";
+        }
     }
 
     // Restart game by reloading the scene
---
+++ Assets/Challenge 5/Scripts/GithubLink.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GithubLink : MonoBehaviour
+{
+    public void OpenLink()
+    {
+        Application.OpenURL("https://github.com/Cillian1234/Challenge-5");
+    }
+}
